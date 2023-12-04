7d04e94d90198e8933321937cb17667f8f9ca457
ethanbockler <ethanbockler@gmail.com>
2023-12-03 18:06:19 -0800

    Combat turn system implemented

---
+++ Assets/Scripts/combat_Turn.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum TurnState
+{
+    Player,
+    Enemy
+}
+
+public class combat_Turn : MonoBehaviour
+{   
+    
+    public float randomNum = 0f;
+    public float currentTime = 0f;
+    public float startTime = 3f;
+    public bool turnOver = false;
+    public TMP_Text turnLabel;
+    public TMP_Text countdown;
+    public TurnState currentTurn = TurnState.Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnLabel.gameObject.SetActive(false);
+        countdown.gameObject.SetActive(false);
+        currentTime = startTime;
+        this.transform.localPosition = new Vector3(370f, 125f, 0f);
+        randomNum = Random.Range(3,7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!turnOver)
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x - 1f, 125f, 0f);
+        }
+        else
+        {
+            currentTime -= 1 * Time.deltaTime;
+            countdown.text = currentTime.ToString("0");
+            if(currentTime <= 0)
+            {
+                countdown.gameObject.SetActive(false);
+                turnLabel.gameObject.SetActive(false);
+                turnOver = false;
+            }
+        }
+
+        if(this.transform.localPosition.x <= -360 && randomNum > 0)
+        {
+            this.transform.localPosition = new Vector3(370f, 125f, 0f);
+            randomNum -= 1;
+        }
+        if(randomNum <= 0)
+        {
+            randomNum = 0;
+            currentTime = 3;
+            SwapTurn();
+            turnOver = true;
+            randomNum = Random.Range(3,7);
+        }
+    }
+
+    void SwapTurn()
+    {
+        if(currentTurn == TurnState.Player)
+        {
+            turnLabel.text = "Opponent's Turn";
+            currentTurn = TurnState.Enemy;
+        }
+        else
+        {
+            turnLabel.text = "Your Turn";
+            currentTurn = TurnState.Player;
+        }
+
+        turnLabel.gameObject.SetActive(true);
+        countdown.gameObject.SetActive(true);
+        
+    }
+}
